e958d66c4b9d7858046d6200303176358214ab7a
lotsonL09 <willimaca09ac@hotmail.com>
2022-06-19 13:25:49 -0500

    Conexión con la tabla de la base de datos

--- Pagina_app/models.py
+++ Pagina_app/models.py
@@ -5,3 +5,16 @@ from django.db import models
 class Curso(models.Model):
     nombre=models.CharField(max_length=30)
     creditos=models.PositiveSmallIntegerField()
+
+class Measurements(models.Model):
+    date = models.DateField(blank=True, null=True)
+    time = models.TimeField(blank=True, null=True)
+    prototype_id = models.IntegerField(blank=True, null=True)
+    temperature = models.FloatField(blank=True, null=True)
+    humidity = models.FloatField(blank=True, null=True)
+    gas = models.FloatField(blank=True, null=True)
+    pm = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'measurements'
--- Pagina_app/views.py
+++ Pagina_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import Curso
+from .models import Measurements
 
 # Create your views here.
 def inicio(request):
     #cursos_listados=Curso.objects.all()[:5]
-    #cursos_listados=Curso.objects.all().order_by('-nombre')
+    cursos_listados=Measurements.objects.all()
     #cursos_listados=Curso.objects.all().order_by('creditos')
     #cursos_listados=Curso.objects.filter(nombre='CDI')
     #cursos_listados=Curso.objects.filter(creditos__gte=4) #creditos mayores a cuatro lte para menores a
-    cursos_listados=Curso.objects.filter(nombre__contains='I')
+    #cursos_listados=Curso.objects.filter(nombre__contains='I')
     return render(request, 'paginas/gestion_curso.html',{"cursos":cursos_listados} )
