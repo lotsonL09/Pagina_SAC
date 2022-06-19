cc49ed908a0f37e4aa75744b2bbe9c923acbb7e4
lotsonL09 <willimaca09ac@hotmail.com>
2022-06-19 13:01:17 -0500

    Practica de ORM

--- Pagina_app/views.py
+++ Pagina_app/views.py
@@ -4,6 +4,9 @@ from .models import Curso
 # Create your views here.
 def inicio(request):
     #cursos_listados=Curso.objects.all()[:5]
-    #cursos_listados=Curso.objects.all().order_by('')
+    #cursos_listados=Curso.objects.all().order_by('-nombre')
+    #cursos_listados=Curso.objects.all().order_by('creditos')
+    #cursos_listados=Curso.objects.filter(nombre='CDI')
+    #cursos_listados=Curso.objects.filter(creditos__gte=4) #creditos mayores a cuatro lte para menores a
+    cursos_listados=Curso.objects.filter(nombre__contains='I')
     return render(request, 'paginas/gestion_curso.html',{"cursos":cursos_listados} )
-
